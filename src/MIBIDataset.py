beb98b20248b693d5b392dd499f16f1d83b27a22
Ishita <ishita@DNa821524.SUNet>
2023-05-31 13:21:47 -0700

    changes to MIBIDataset

--- src/MIBIDataset.py
+++ src/MIBIDataset.py
@@ -3,20 +3,21 @@ import torch
 from torch.utils.data import Dataset
 
 class MIBIDataset(Dataset):
-    def __init__(self, similar_samples, dissimilar_samples):
-        self.similar_samples = similar_samples
-        self.dissimilar_samples = dissimilar_samples
+    def __init__(self, similar_samples=None, dissimilar_samples=None, transform=None):
+        self.sim_df=pd.read_csv(similar_samples)
+        self.sim_df.columns =["Tile_1","Tile_2","Label"]
+        self.dissim_df=pd.read_csv(dissimilar_samples)
+        self.dissim_df.columns =["Tile_1","Tile_2","Label"]
+        self.transform = transform
 
     def __getitem__(self, index):
         if index < len(self.similar_samples):
             # Fetch a pair of similar samples
-            sample1, sample2 = self.similar_samples[index]
-            label = torch.tensor(0)  # Similar pair label is 0
+            sample1, sample2, label = self.sim_df[index]
         else:
             # Fetch a pair of dissimilar samples
             index -= len(self.similar_samples)
-            sample1, sample2 = self.dissimilar_samples[index]
-            label = torch.tensor(1)  # Dissimilar pair label is 1
+            sample1, sample2, label = self.dissim_df[index]
 
         return sample1, sample2, label
 
